4a53f30483bb19383171395e77ae33ea6e0f302f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-04 16:10:29 +0300

    Create sequelize

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer();
+
+server.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
+});
