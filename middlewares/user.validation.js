f59cf565b350fe2b31dc8d9a2fee5f442c123642
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-10 19:18:06 +0300

    Add validation

--- middlewares/user.validation.js
+++ middlewares/user.validation.js
@@ -1,20 +1,7 @@
-const yup = require("yup");
-const { User } = require("../models");
+const {USER_SCHEMA} = require('../schemas/user.schema')
 
-const USER_SCHEMA = yup.object({
-  firstName: yup.string().required().min(1),
-  lastName: yup.string().required().min(1),
-  email: yup.string().required().email("Invalid email"),
-  password: yup
-    .string()
-    .required()
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
-      "Пароль має бути мінімум 8 символів, містити велику та малу літеру, цифру і спецсимвол"
-    ),
-  birthday: yup.date(),
-  gender: yup.string(),
-});
+
+
 
 module.exports.validateUser = async (req, res, next) => {
   try {
