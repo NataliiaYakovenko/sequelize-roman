55556147f39fea8e8bb5318a5705a153de1e6895
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-12 22:06:57 +0300

    Did migration

---
+++ migrations/20250812183042-users_to_groups.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users_to_groups", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        field: "user_id",
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      groupId: {
+        field: "group_id",
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "groups",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users_to_groups");
+  },
+};
